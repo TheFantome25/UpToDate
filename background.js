300ea7b3e89e352a4669c7d3105e5f556ce0155e
disdero <eric_disdero@hotmail.com>
2023-07-26 01:35:27 +0200

    working

--- background.js
+++ background.js
@@ -1,27 +1,22 @@
-var favoritesList = new Array();
-var activeTabs = new Array();
+/*
+This script is the backbone of the extention: 
+Keep the url of your opened bookmarks updated when you advance in your reading
+using addListener on chrome.tabs.onUpdated 
+*/
+
+//The list of all your bookmarks
+var upToDateBookmarks = new Array();
+
+//The map of tab if with the coresponding bookmark oppen inside it.
+const activeBookmarksbyId = new Map();
 
 //Loads bookmarks from the specific folder TheScans
 chrome.bookmarks.getTree(function (bookmarkTreeNodes) {
-  var folder = findFolder(bookmarkTreeNodes[0], "TheScans");
+  var folder = findFolder(bookmarkTreeNodes[0], "UpToDate");
   if (folder) {
     addAllFolder(folder);
   }
 });
-
-
-function addAllFolder(node) {
-  if (node.children) {
-    for (var i = 0; i < node.children.length; i++) {
-      //Start recursive search
-      addAllFolder(node.children[i]);
-    }
-  } else {
-    favoritesList.push(node.url);
-  }
-}
-
-
 
 //Recursive methode to find a folder by name
 function findFolder(node, folderName) {
@@ -36,17 +31,33 @@ function findFolder(node, folderName) {
       }
     }
   }
-  return null;
+  return null;//Should be the creation of the said folder...
 }
+
+//Adds all the bookmarks even if you have multiple subfolders
+function addAllFolder(node) {
+  if (node.children) {
+    for (var i = 0; i < node.children.length; i++) {
+      //Start recursive search
+      addAllFolder(node.children[i]);
+    }
+  } else {
+    upToDateBookmarks.push(node);
+  }
+}
+
+
+
+
 
 //If tab is created with url from out folder then : we keep track of it and it's url changes.
 chrome.tabs.onCreated.addListener(function (tab) {
   // This function will be called when a new tab is created.
   // Pending url will be the one to open the tab
-  favoritesList.forEach(element => {
-    if (tab.pendingUrl === element) {
-      isInFav = true;
-      activeTabs.push(tab.id);
+  upToDateBookmarks.forEach(element => {
+    if (tab.pendingUrl === element.url) {
+      activeBookmarksbyId.set(tab.id, element);
+      console.log("onCreated.addListener " + tab.pendingUrl);
     }
   });
 });
@@ -55,95 +66,31 @@ chrome.tabs.onCreated.addListener(function (tab) {
 chrome.tabs.onRemoved.addListener(function (tab) {
   // This function will be called when a new tab is created.
   // Pending url will be the one to open the tab
-  activeTabs.forEach(tab => {
-    if (tab.id === tab) {
-      activeTabs.pop(tab.id);
-    }
-  });
-
+  if (activeBookmarksbyId.has(tab.id)) {
+    activeBookmarksbyId.delete(tab.id);
+  }
 });
 
+
+//Hapens chen a tab has a change in URL:
+// It is here that we want to update the favorite if it is an active tab (avoid looking at every tabs and every url redirect)
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  
-  console.log("addListener");
-  activeTabs.forEach(element => {
-      console.log(activeTabs);    
-  });
-  console.log(changeInfo.url);
-  console.log(changeInfo.url);
-  console.log(tab.url);
-  console.log(tab.pendingUrl);
+  if (activeBookmarksbyId.has(tabId)) {
+    if (changeInfo.url !== "") {
+      let updatedBookmark = activeBookmarksbyId.get(tabId);
+      updatedBookmark.url = changeInfo.url
 
-  // Check if the URL changed and if there was a redirect
-    var isActiveTab = false;
-    activeTabs.forEach(element => {
-      if (tabId === element) {
-        isActiveTab = true;
-      }
-    });
+      const twoPropBookmark = {
+        title: updatedBookmark.title,
+        url: updatedBookmark.url
+      };
 
-    if (isInFav) {
-      console.log('We must find the good favorite and update it...');
-
+      // Update the bookmark using the chrome.bookmarks.update method
+      chrome.bookmarks.update(updatedBookmark.id, twoPropBookmark, () => {
+        // Bookmark updated successfully
+        console.log("Bookmark updated successfully");
+      });
     }
   }
-
+}
 );
-
-
-// chrome.runtime.onInstalled.addListener(function() {
-//   chrome.bookmarks.create({title: 'My Favorites'}, function(bookmarkFolder) {
-//     console.log('Created bookmark folder: ' + bookmarkFolder.title);
-//   });
-// });
-
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//   if (request.message === 'addFavorite') {
-//     chrome.bookmarks.search({title: 'My Favorites'}, function(bookmarks) {
-//       if (bookmarks.length > 0) {
-//         var bookmarkFolderId = bookmarks[0].id;
-//         chrome.bookmarks.create({parentId: bookmarkFolderId, title: request.url, url: request.url}, function(bookmark) {
-//           console.log('Added favorite: ' + bookmark.title);
-//           sendResponse({message: 'success'});
-//         });
-//       }
-//     });
-//     return true;
-//   }
-//   if (request.message === 'removeFavorite') {
-//     chrome.bookmarks.search({title: 'My Favorites'}, function(bookmarks) {
-//       if (bookmarks.length > 0) {
-//         var bookmarkFolderId = bookmarks[0].id;
-//         chrome.bookmarks.search({parentId: bookmarkFolderId, title: request.url}, function(bookmarks) {
-//           if (bookmarks.length > 0) {
-//             var bookmarkId = bookmarks[0].id;
-//             chrome.bookmarks.remove(bookmarkId, function() {
-//               console.log('Removed favorite: ' + request.url);
-//               sendResponse({message: 'success'});
-//             });
-//           }
-//         });
-//       }
-//     });
-//     return true;
-//   }
-// });
-
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-//   if (changeInfo.url) {
-//     chrome.bookmarks.search({title: 'My Favorites'}, function(bookmarks) {
-//       if (bookmarks.length > 0) {
-//         var bookmarkFolderId = bookmarks[0].id;
-//         chrome.bookmarks.getChildren(bookmarkFolderId, function(children) {
-//           for (var i = 0; i < children.length; i++) {
-//             var child = children[i];
-//             if (child.url === changeInfo.url) {
-//               chrome.bookmarks.update(child.id, {title: tab.title});
-//             }
-//           }
-//         });
-//       }
-//     });
-//   }
-// });
-
