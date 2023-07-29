ec6512337b83288456ef2fcfcd767172f3c4d5a6
disdero <eric_disdero@hotmail.com>
2023-07-29 22:20:35 +0200

    wip gestion bookmarks

--- background.js
+++ background.js
@@ -5,29 +5,18 @@ using addListener on chrome.tabs.onUpdated
 */
 
 
-//Original point before change due to the plugin
-var initialPointBookmarks = new Map();
-
-
 //The list of all your bookmarks
 var upToDateBookmarks = new Map();
 
 
 //The list of folders/subfolders that are inside UpToDate
-var folder = new Array();
+var idsOfBookmarksInGoodFolder = new Array();
 
 
-//The map of tab if with the coresponding bookmark oppen inside it.
-var activeBookmarksbyId = new Map();
-
-
+//The list of tabs that the plugin is activave on
+var activeTabsId = new Set();
 //The list of tabs that the plugin cannot activate on
 var excludedTabs = new Map();
-
-
-//The list of tabs that the plugin is activated on
-var activeTabs = new Map();
-
 
 
 
@@ -38,7 +27,7 @@ initializeAllFolderAndBookmarks();
 //Loads bookmarks from the specific folder TheScans
 function initializeAllFolderAndBookmarks() {
   upToDateBookmarks = new Map();
-  folder = new Array();
+  idsOfBookmarksInGoodFolder = new Array();
 
   chrome.bookmarks.getTree(function (bookmarkTreeNodes) {
     var folder = findFolder(bookmarkTreeNodes[0], "UpToDate");
@@ -64,46 +53,32 @@ function findFolder(node, folderName) {
       }
     }
   }
-  return null;//Should be the creation of the said folder...
+  return null;
 }
 
 
 //Adds all the bookmarks even if you have multiple subfolders
 function addAllFoldersAndBookmarks(node) {
-  if (!node.url) {
-    //It is a folder we add it to this list
-    folder.push(folder.id)
+  idsOfBookmarksInGoodFolder.push(node.id)
 
+  if (!node.url) {
     for (var i = 0; i < node.children.length; i++) {
       //Start recursive search
       addAllFoldersAndBookmarks(node.children[i]);
     }
   } else {
-    let startURL=node.title.split("|")[1];
-    upToDateBookmarks.set(startURL, node);
+    let startURL = node.title.split("|")[1];
+    if (startURL !== undefined) { upToDateBookmarks.set(startURL, node); }
   }
 }
 
 
 //If tab is created with url from out folder then : we keep track of it and it's url changes.
 chrome.tabs.onCreated.addListener(function (tab) {
-  attachingAndTrackingTab(tab, tab.pendingUrl)
 
 });
 
-//activate if not disabled on that special tab(not here)
 
-//check if the url is an existing element that we can folow
-
-
-function attachingAndTrackingTab(tab, urlToCheck) {
-  // This function will be called when a new tab is created.
-  // Pending url will be the one to open the tab
-  if (getMatchingUrlBookmark(urlToCheck) != undefined) {
-    activeTabs.set(tab.id, tab)
-    console.log("onCreated.addListener tabid: " + tab.id + " tab pending url :" + tab.pendingUrl);
-  }
-}
 
 
 function getMatchingUrlBookmark(urlToFind) {
@@ -122,9 +97,7 @@ function getMatchingUrlBookmark(urlToFind) {
 chrome.tabs.onRemoved.addListener(function (tab) {
   // This function will be called when a new tab is created.
   // Pending url will be the one to open the tab
-  if (activeTabs.has(tab.id)) {
-    activeTabs.delete(tag.id);
-  }
+
 });
 
 
@@ -132,76 +105,62 @@ chrome.tabs.onRemoved.addListener(function (tab) {
 //Hapens chen a tab has a change in URL:
 // It is here that we want to update the favorite if it is an active tab (avoid looking at every tabs and every url redirect)
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.url !== undefined && !excludedTabs.has(tabId)) {
-      console.log("Modification of url detected changeInfo.url: " + changeInfo.url + " tab.url: " + tab.url);
-
-    var bookmark = getMatchingUrlBookmark(changeInfo.url);
-    if (bookmark != undefined) {
-      activeTabs.set(tab.id, tab)
-      // Update the bookmark using the chrome.bookmarks.update method
-      var newURlPropBookmark = {
-        url: changeInfo.url
-      };
-      chrome.bookmarks.update(bookmark.id, newURlPropBookmark, () => {
-        // Bookmark updated successfully
-        console.log("Bookmark updated successfully tabid: " + tabId + " tab url: " + tab.url + " tab title " + tab.title);
-      });
-    } else {
-      if (activeTabs.has(tabId)) {
-        activeTabs.delete(tabId);
+  if (changeInfo.url != undefined) {
+    if (!excludedTabs.has(tabId)) {
+      var bookmark = getMatchingUrlBookmark(changeInfo.url);
+      if (bookmark != undefined) {
+        // Update the bookmark using the chrome.bookmarks.update method
+        var newURlPropBookmark = {
+          url: changeInfo.url
+        };
+        chrome.bookmarks.update(bookmark.id, newURlPropBookmark, () => {
+          activeTabsId.add(tabId);
+        });
+      } else {
+        activeTabsId.delete(tabId);
       }
     }
+  }
+
+  if (activeTabsId.has(tabId)) {
+    setExtensionIcon(tabId, true);
+  } else {
+    setExtensionIcon(tabId, false);
   }
 });
 
 
-  // console.log("Modification of url detected changeInfo.url: " + changeInfo.url + " tab.url: " + tab.url);
-  // console.log("State of condition url defined " + (changeInfo.url !== undefined));
-
-  // let updatedBookmark = activeBookmarksbyId.get(tabId);
-  // console.log("State of condition nouvelle url " + (updatedBookmark.url !== changeInfo.url));
-
-  // if (changeInfo.url !== undefined && updatedBookmark.url !== changeInfo.url) {
-  //   updatedBookmark.url = changeInfo.url
-
-  //   console.log("We are in");
-
-  //   const newURlPropBookmark = {
-  //     url: updatedBookmark.url
-  //   };
-
-  //   // Update the bookmark using the chrome.bookmarks.update method
-  //   chrome.bookmarks.update(updatedBookmark.id, newURlPropBookmark, () => {
-  //     // Bookmark updated successfully
-  //     console.log("Bookmark updated successfully tabid: " + tabId + " tab url: " + tab.url + " tab title " + tab.title);
-  //   });
-  // }
-
-
-  // if (activeBookmarksbyId.has(tabId)) {
-  //   console.log("Bookmark modification of url detected changeInfo.url: " + changeInfo.url + " tab.url: " + tab.url);
-  //   console.log("State of condition url defined " + (changeInfo.url !== undefined));
-
-  //   let updatedBookmark = activeBookmarksbyId.get(tabId);
-  //   console.log("State of condition nouvelle url " + (updatedBookmark.url !== changeInfo.url));
-
-  //   if (changeInfo.url !== undefined && updatedBookmark.url !== changeInfo.url) {
-  //     updatedBookmark.url = changeInfo.url
-
-  //     console.log("We are in");
-
-  //     const newURlPropBookmark = {
-  //       url: updatedBookmark.url
-  //     };
-
-  //     // Update the bookmark using the chrome.bookmarks.update method
-  //     chrome.bookmarks.update(updatedBookmark.id, newURlPropBookmark, () => {
-  //       // Bookmark updated successfully
-  //       console.log("Bookmark updated successfully tabid: " + tabId + " tab url: " + tab.url + " tab title " + tab.title);
-  //     });
-  //   }
-  // }
-//});
+async function setExtensionIcon(tabId, active) {
+  if (active) {
+    try {
+      await chrome.browserAction.setBadgeText({
+        tabId: tabId,
+        text: "1",
+      });
+      await chrome.browserAction.setBadgeBackgroundColor({
+        tabId: tabId,
+        color: "#156924"
+      });
+    }
+    catch (reason) {
+      console.log(reason);
+    }
+  } else {
+    try {
+      await chrome.browserAction.setBadgeText({
+        tabId: tabId,
+        text: "0",
+      });
+      await chrome.browserAction.setBadgeBackgroundColor({
+        tabId: tabId,
+        color: "#363636"
+      });
+    }
+    catch (reason) {
+      console.log(reason);
+    }
+  }
+}
 
 
 
@@ -209,57 +168,102 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 
 
 
-// function handleBookmarkCreated(id, bookmarkInfo) {
-//   //Check if it is a bookmark of our elements
-//   if (folder.includes(bookmarkInfo.parentId)) {
-//     if (!bookmarkInfo.url) {
-//       folder.push(id)
-//     } else {
 
-//       upToDateBookmarks.push(bookmarkInfo)
 
-//       chrome.tabs.getCurrent(function (tab) {
-//         attachingAndTrackingTab(tab, tab.url);
-//         console.log("Bookmark Created:", bookmarkInfo);
-//       });
-//     }
-//   }
-// }
 
-// // Function to handle bookmark removal
-// function handleBookmarkRemoved(id, removeInfo) {
-//    //Check if it is a bookmark of our elements
-//    if (folder.includes(bookmarkInfo.parentId)) {
-//     if (!bookmarkInfo.url) {
-//       folder.delete(id)
-//     } else {
 
-//       upToDateBookmarks.push(bookmarkInfo)
 
-//       chrome.tabs.getCurrent(function (tab) {
-//         attachingAndTrackingTab(tab, tab.url);
-//         console.log("Bookmark Created:", bookmarkInfo);
-//       });
-//     }
-//   }
-//   console.log("Bookmark Removed:", removeInfo);
-//   //check if it is inside the good folder, if yes than remove it from upToDateBookmarks
-// }
 
-// // Function to handle bookmark changes (e.g., title or URL change)
-// function handleBookmarkChanged(id, changeInfo) {
-//   console.log("Bookmark Changed:", changeInfo);
-//   //check if it is inside the good folder, if yes than update it from upToDateBookmarks
-// }
 
-// // Function to handle bookmark movement within the bookmark tree
-// function handleBookmarkMoved(id, moveInfo) {
-//   console.log("Bookmark Moved:", moveInfo);
-//   //check if it is inside the good folder, update upToDateBookmarks accordingly
-// }
+
+
+
+
+
+
+chrome.bookmarks.onCreated.addListener(handleBookmarkCreated);
+//Section bookmarks events listener
+function handleBookmarkCreated(id, bookmarkInfo) {
+  //Check if it is a bookmark of our elements
+  if (idsOfBookmarksInGoodFolder.includes(bookmarkInfo.parentId)) {
+    console.log("bookmark of interest created or moved")
+
+    idsOfBookmarksInGoodFolder.push(id);
+
+    let startURL = bookmarkInfo.title.split("|")[1];
+    if (startURL !== undefined) {
+      upToDateBookmarks.set(startURL, bookmarkInfo);
+    }
+
+  }
+}
+
+chrome.bookmarks.onRemoved.addListener(handleBookmarkRemoved);
+// Function to handle bookmark removal
+function handleBookmarkRemoved(id, removeInfo) {
+  //Check if it is a bookmark of our elements
+  if (idsOfBookmarksInGoodFolder.includes(removeInfo.parentId)) {
+    if (!removeInfo.node.url) {
+      idsOfBookmarksInGoodFolder.delete(id)
+    } else {
+      let startURL = removeInfo.node.title.split("|")[1];
+      upToDateBookmarks.delete(startURL);
+    }
+  }
+}
+
+
+chrome.bookmarks.onChanged.addListener(handleBookmarkChanged);
+function handleBookmarkChanged(id, changeInfo) {
+  if (idsOfBookmarksInGoodFolder.includes(id)) {
+    //if it is in the list : modify it
+    var bookmarkInActiveList;
+    upToDateBookmarks.forEach((value) => {
+      if (value.id === id) {
+        bookmarkInActiveList = value;
+      }    //the only case of interest would be a change that the user made in the bookmars and not in navigating
+
+    });
+    if (bookmarkInActiveList !== undefined && changeInfo.title.split("|")[1] !== undefined) {
+      if (bookmarkInActiveList.url !== changeInfo.url || bookmarkInActiveList.title !== changeInfo.title) {
+        bookmarkInActiveList.title = changeInfo.title.split("|")[1];
+        bookmarkInActiveList.url = changeInfo.url;
+        console.log("bookmark modified by user and not program")
+      }
+    } else {
+      if (changeInfo.title.split("|")[1] !== undefined && changeInfo.url !== undefined) {
+        chrome.bookmarks.get(id, (bookmark) => {
+          if (bookmark !== undefined) {
+            upToDateBookmarks.set(changeInfo.title.split("|")[1], bookmark);
+          }
+        });
+
+
+      }
+
+
+    }
+
+  }
+}
+
+//Tu change un élément: il était mal répertorié mauvais titre
+
+
+
+chrome.bookmarks.onMoved.addListener(handleBookmarkMoved);
+// Function to handle bookmark movement within the bookmark tree
+function handleBookmarkMoved(id, moveInfo) {
+  if (!idsOfBookmarksInGoodFolder.includes(moveInfo.oldParentId) && idsOfBookmarksInGoodFolder.includes(moveInfo.parentId)) {
+
+  }
+  if (folder.includes(bookmarkInfo.parentId)) {
+    //handleBookmarkCreated(id,moveInfo);
+
+  } else if (true) {
+    //handleBookmarkRemoved(id,moveInfo);
+  }
+
+}
 
 // // Add event listeners to monitor bookmark changes
-// chrome.bookmarks.onCreated.addListener(handleBookmarkCreated);
-// chrome.bookmarks.onRemoved.addListener(handleBookmarkRemoved);
-// chrome.bookmarks.onChanged.addListener(handleBookmarkChanged);
-// chrome.bookmarks.onMoved.addListener(handleBookmarkMoved);
